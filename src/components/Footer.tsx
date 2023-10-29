d55eaf18a9e6725c77af0ca841549a90e4f87251
thao <mrtrados@gmail.com>
2023-10-29 22:14:59 +0900

    UPDATE footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -30,13 +30,13 @@ export function Footer() {
             <footer className="container font-display text-grey-darkest mx-auto px-4 text-xs mt-24 mb-12 tracking-wide text-center">
                 <nav className="w-full mb-4">
                     <ul className="flex justify-center tracking-wide space-x-4 mb-2">
-                        {siteMap}
+                        {/* {siteMap} */}
                     </ul>
                     <ul className="flex justify-center tracking-wide space-x-4">
-                        {socials}
+                        {/* {socials} */}
                     </ul>
                 </nav>
-                <p>©{year} Thao</p>
+                <p>©{year} Charlie Thao</p>
             </footer>
         </>
     )
